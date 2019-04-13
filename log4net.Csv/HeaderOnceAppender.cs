f3f5e13a87fed3f1c1f355877814d6d3720908f6
bablovLi <bab_lov@hotmail.com>
2019-04-13 22:48:07 +0800

    Include header only once at the top of a rolling file

---
+++ log4net.Csv/HeaderOnceAppender.cs
@@ -0,0 +1,27 @@
+﻿using log4net.Appender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net.Csv
+{
+    public class HeaderOnceAppender : RollingFileAppender
+    {
+        protected override void WriteHeader()
+        {
+            try
+            {
+                if (LockingModel.AcquireLock().Length == 0)
+                {
+                    base.WriteHeader();
+                }
+            }
+            finally
+            {
+                LockingModel.ReleaseLock();
+            }
+        }
+    }
+}
